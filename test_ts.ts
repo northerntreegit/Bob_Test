2692dc3ac29449b157e1509deee79e15527c2f98
northerntreegit <55422582+northerntreegit@users.noreply.github.com>
2025-08-29 21:52:55 +0800

    Add bobtest namespace with testFunction_TS

---
+++ test_ts.ts
@@ -0,0 +1,8 @@
+//% color=#E3008C weight=100 icon="\uf1d8" block="Bob Test"
+namespace bobtest {
+    //% block="TestVersion 1"
+    //% weight=100
+    export function testFunction_TS(): void {
+    }
+
+}
