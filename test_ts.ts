da53b1f468f03c04dbf94c7b1f78fa10ce531586
northerntreegit <55422582+northerntreegit@users.noreply.github.com>
2025-08-31 03:30:22 +0800

    Update test_ts.ts

--- test_ts.ts
+++ test_ts.ts
@@ -1,6 +1,6 @@
 //% color=#E3008C weight=100 icon="\uf1d8" block="Bob Test"
 namespace bobtest {
-    //% block="TestVersion 1"
+    //% block="Test 1"
     //% weight=100
     export function testFunction_TS(): void {
     }
