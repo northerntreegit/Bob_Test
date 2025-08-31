54fc0f5e08897c34ea0907f7c79098b1c8f761c6
northerntreegit <55422582+northerntreegit@users.noreply.github.com>
2025-08-31 23:48:57 +0800

    Rename test blocks in bobtest namespace

--- test_ts.ts
+++ test_ts.ts
@@ -1,12 +1,12 @@
 //% color=#E3008C weight=100 icon="\uf1d8" block="Bob Test"
 namespace bobtest {
-    //% block="Test 1"
+    //% block="Test a"
     //% weight=100
     export function testFunction_TS(): void {
     }
 
 
-    //% block="Test 2"
+    //% block="Test b"
     //% weight=90
     export function testFunction_TS2(): void {
         bobtest.TTT_C();
